58e615b7893020bdf10f0a0be1fcced911e59b0c
franbujaldon <f.bujaldon@alumno.um.edu.ar>
2023-04-10 21:28:44 -0300

    Decimal to roman Implementacion

---
+++ ToRomanST.py
@@ -0,0 +1,18 @@
+
+
+def decimal_to_roman(decimal):
+    total = ''
+    if decimal >= 100:
+        centena = decimal // 100
+        total = 'C' * centena
+        decimal = decimal % 100 
+    if decimal <= 3:
+        total += 'I' * decimal
+    elif decimal == 5:
+        total += 'V'
+    elif decimal == 10:
+        total += "X"
+    return total
+
+
+
